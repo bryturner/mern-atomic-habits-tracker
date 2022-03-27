87578e941e27689d73646866a62305e6276f19cc
bryan.turner.ch@gmail.com <bryan.turner.ch@gmail.com>
2022-03-26 19:40:55 -0600

    index inputs and buttons added

---
+++ client/src/components/buttons/index.js
@@ -0,0 +1,3 @@
+export { default as CloseHabitFormButton } from './CloseHabitFormButton';
+export { default as AddHabitButton } from './AddHabitButton';
+export { default as DeleteHabitButton } from './DeleteHabitButton';
--- client/src/components/inputs/index.js
+++ client/src/components/inputs/index.js
@@ -1,2 +1,3 @@
 export { default as CheckedCheckbox } from './CheckedCheckbox';
 export { default as UncheckedCheckbox } from './UncheckedCheckbox';
+export { default as TextInput } from './TextInput';
